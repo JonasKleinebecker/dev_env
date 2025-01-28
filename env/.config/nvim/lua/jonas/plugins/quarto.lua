72a54accf926ed8daeaf76ef4105b637b7a61d32
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2025-01-28 23:51:22 +0100

    feat: add quarto keymaps

--- env/.config/nvim/lua/jonas/plugins/quarto.lua
+++ env/.config/nvim/lua/jonas/plugins/quarto.lua
@@ -4,5 +4,29 @@ return {
 	dependencies = {
 		"jmbuhr/otter.nvim",
 		opts = {},
+		config = function()
+			local quarto = require("quarto")
+			quarto.setup()
+
+			vim.keymap.set({ "n", "v" }, "<m-i>", "<esc>i```{python}<cr>```<esc>O", { desc = "Insert python block" })
+			vim.keymap.set(
+				"n",
+				"<leader>qp",
+				quarto.quartoPreview,
+				{ silent = true, noremap = true, desc = "Quarto preview" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>qx",
+				quarto.quartoClosePreview,
+				{ silent = true, noremap = true, desc = "Quarto close preview" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>qa",
+				quarto.activate,
+				{ silent = true, noremap = true, desc = "Quarto activate" }
+			)
+		end,
 	},
 }
