2087b03ba1847ad0538079075f32eed47bf8dc5a
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2025-02-05 20:03:47 +0100

    improve telescope keymaps

--- env/.config/nvim/lua/jonas/plugins/telescope.lua
+++ env/.config/nvim/lua/jonas/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local tel_builtin = require("telescope.builtin")
 		local transform_mod = require("telescope.actions.mt").transform_mod
 
 		local trouble = require("trouble")
@@ -29,10 +30,13 @@ return {
 					i = {
 						["<M-k>"] = actions.move_selection_previous, -- move to prev result
 						["<M-j>"] = actions.move_selection_next, -- move to next result
+						["<C-c>"] = actions.close,
 					},
 					n = {
 						["<M-k>"] = actions.move_selection_previous, -- move to prev result
 						["<M-j>"] = actions.move_selection_next, -- move to next result
+						["<C-c>"] = actions.close,
+						["q"] = actions.close,
 					},
 				},
 			},
@@ -48,5 +52,15 @@ return {
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+		keymap.set("n", "<leader>fF", function()
+			tel_builtin.find_files({ hidden = true })
+		end, { desc = "Fuzzy find files in cwd, including hidden files" })
+		keymap.set("n", "<ledaer>fS", function()
+			tel_builtin.live_grep({ hidden = true })
+		end, { desc = "Find string in cwd, including hidden files" })
+		keymap.set("n", "<leader>fC", function()
+			tel_builtin.grep_string({ hidden = true })
+		end, { desc = "Find string under cursor in cwd, including hidden files" })
 	end,
 }
