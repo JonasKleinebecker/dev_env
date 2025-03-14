d12e22d00ce1bfb5a0daaabda8381f0b1e43a392
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2025-03-14 16:07:19 +0100

    Add null check for venv in mypy configuration

--- env/.config/nvim/lua/jonas/plugins/lsp/none-ls.lua
+++ env/.config/nvim/lua/jonas/plugins/lsp/none-ls.lua
@@ -1,65 +1,65 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-		"jayp0521/mason-null-ls.nvim",
-	},
-	config = function()
-		require("mason-null-ls").setup({
-			ensure_installed = { "ruff", "prettier", "shfmt", "mypy", "stylua" },
-			automatic_installation = true,
-		})
-		local nls = require("null-ls")
-		local sources = {
-			require("none-ls.formatting.ruff").with({
-				extra_args = {
-					"--extend-select",
-					"I",
-				},
-			}),
-			require("none-ls.formatting.ruff_format"),
-			nls.builtins.diagnostics.mypy.with({
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim",
+  },
+  config = function()
+    require("mason-null-ls").setup({
+      ensure_installed = { "ruff", "prettier", "shfmt", "mypy", "stylua" },
+      automatic_installation = true,
+    })
+    local nls = require("null-ls")
+    local sources = {
+      require("none-ls.formatting.ruff").with({
+        extra_args = {
+          "--extend-select",
+          "I",
+        },
+      }),
+      require("none-ls.formatting.ruff_format"),
+      nls.builtins.diagnostics.mypy.with({
         extra_args = function()
-        local args = {
+          local args = {
             "--disable-error-code=attr-defined",
             "--disable-error-code=name-defined",
-        }
-        -- Only add python-executable if virtual env exists
-        local venv = vim.fn.getenv("VIRTUAL_ENV")
-        if venv and venv ~= "" then
+          }
+          -- Only add python-executable if virtual env exists
+          local venv = vim.fn.getenv("VIRTUAL_ENV")
+          if type(venv) == "string" and venv ~= "" then
             table.insert(args, "--python-executable=" .. venv .. "/bin/python")
-        end
-        return args
-    end,
-				runtime_condition = function(params)
+          end
+          return args
+        end,
+        runtime_condition = function(params)
           local full_path = vim.api.nvim_buf_get_name(params.bufnr)
           return not (full_path:match(".venv/") or full_path:match("venv/"))
-				end,
-			}),
+        end,
+      }),
       nls.builtins.formatting.stylua,
-			nls.builtins.formatting.prettier.with({
-				filetypes = {
-					"json",
-					"yaml",
-					"markdown",
-				},
-			}),
-		}
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		nls.setup({
-			sources = sources,
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-		})
-	end,
+      nls.builtins.formatting.prettier.with({
+        filetypes = {
+          "json",
+          "yaml",
+          "markdown",
+        },
+      }),
+    }
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    nls.setup({
+      sources = sources,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
