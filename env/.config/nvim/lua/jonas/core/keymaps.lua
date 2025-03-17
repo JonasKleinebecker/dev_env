1a4e6f7dbfcc04d00261664f4b19eb1bb7ed2ce1
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2025-03-18 00:08:03 +0100

    Add toggle markdown checkbox function and keymap

--- env/.config/nvim/lua/jonas/core/keymaps.lua
+++ env/.config/nvim/lua/jonas/core/keymaps.lua
@@ -39,9 +39,9 @@ keymap.set("n", "<leader>vm", "<cmd>Mason<CR>", { silent = true, desc = "Mason" 
 keymap.set("n", "<leader>vl", "<cmd>Lazy<CR>", { silent = true, desc = "Lazy" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 -- quickfix list
@@ -49,12 +49,28 @@ keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnosti
 
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
+
+function toggle_checkbox()
+  local line = vim.api.nvim_get_current_line()
+
+  if line:match("^%s*-%s*%[ %]") then
+    local new_line = line:gsub("%[ %]", "[x]")
+    vim.api.nvim_set_current_line(new_line)
+  elseif line:match("^%s*-%s*%[x%]") then
+    local new_line = line:gsub("%[x%]", "[ ]")
+    vim.api.nvim_set_current_line(new_line)
+  else
+    print("No checkbox found on this line")
+  end
+end
+
+vim.keymap.set("n", "<leader>tc", toggle_checkbox, { noremap = true, silent = true, desc = "Toggle markdown checkbox" })
 
 -- keymap.set("n", "<leader>tx", "<cmd>bd<CR>", { desc = "Close current Buffer" })
 -- keymap.set("n", "<leader>tn", "<cmd>bn<CR>", { desc = "Go to next Buffer" })
